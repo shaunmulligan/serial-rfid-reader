6d26987c48b8c468a4280a7d4f1832ade4901c45
Keith Chester <kchester@gmail.com>
2015-06-29 15:02:15 -0500

    test: Initial test writing:
    Writing out the initial tests, what I hope to accomplish with the library.
    Trying to use this to outline the actual structure of the code

---
+++ test/test.js
@@ -0,0 +1,80 @@
+var expect = require('chai').expect,
+	RFIDReader = require('../index.js');
+	
+var COM_PORT = 'test';
+	
+console.log("Many of these tests require an open connection to an RFID reader as determined by the --port= argument.");
+
+describe('Initialization', function(){
+	
+	it('should allow a string an object with a baudrate attribute to pass', function(){
+		expect(function(){
+			var tmp = new RFIDReader('something', { baudrate: 57600 });
+		}).to.not.throw();
+	});
+	
+	it('should throw an error if you do not provide a port and a settings object.', function(){
+		expect(function(){
+			var tmp = new RFIDReader();
+		}).to.throw();
+		
+		expect(function(){
+			var tmp2 = new RFIDReader('something');
+		}).to.throw();
+	});
+	
+	it('should allow you to pass a number for baudrate for initialization', function(){
+		expect(function(){
+			var tmp = new RFIDReader('something', 57600);
+		}).to.not.throw();
+	});
+	
+	it('should throw an error if the options object does not specify baudrate', function(){
+		expect(function(){
+			var tmp = new RFIDReader('something', { test: 'fake' });
+		}).to.throw();
+	});
+	
+	it('should throw an error if the port is not a string', function(){
+		expect(function(){
+			var tmp = new RFIDReader({ test: 'fake'}, { baudrate: 57600 });
+		}).to.throw();
+	});
+	
+});
+
+describe('Configuring RFID Reader', function(){
+	
+	it('should allow the setting of _serialParse via the setOnSerialRead function');
+	
+	it('should allow the setting of _getViaRFIDToken via the setGetViaRFIDToken function');
+	
+	it('should allow the setting of _findRFIDToken via the setFindRFIDToken function');
+	
+});
+
+describe('Open Connection', function(){
+	
+	it('should throw an error if the serialport connection can not be opened', function(){
+		expect(function(){
+			var tmp = new RFIDReader('fakeSerialPortAddress', 57600);
+			
+			tmp.listen();
+			
+		}).to.throw();
+	});
+	
+	it('should open a connection and not throw any errors if the serial port connection can be opened', function(){
+		expect(function(){
+			var tmp = new RFIDReader(COM_PORT, 57600);
+			
+			tmp.listen();
+			
+		}).to.not.throw();
+	});
+	
+});
+
+describe('Response from card reading', function(){
+	
+});
